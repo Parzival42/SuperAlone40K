8e9ccc817d13c3f7df2eec7df256046e5a6ed63e
Parzival Legmaier <legmaier@gmx.at>
2017-12-05 13:38:33 +0100

    Added note to tween listener

--- SuperAlone40k/src/superAlone40k/util/TweenListener.java
+++ SuperAlone40k/src/superAlone40k/util/TweenListener.java
@@ -1,5 +1,11 @@
 package superAlone40k.util;
 
+import java.awt.event.ActionListener;
+
+/**
+ * Use {@link ActionListener} instead of own interface!
+ * Save a file :)
+ */
 public interface TweenListener {
     void tweenIsFinished();
 }
